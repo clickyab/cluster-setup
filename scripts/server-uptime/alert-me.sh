4bd415025a3345bbd5c276a8f585252e61110653
vahit <vahid.maani@gmail.com>
2018-06-27 11:46:53 +0430

    now alert-me script can send an sms

--- scripts/server-uptime/alert-me.sh
+++ scripts/server-uptime/alert-me.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+EMAIL_RECEPTOR="sysadmin@clickyab.com"
+SMS_RECEPTOR="09107588522, 09124966428"
+KN_API_KEY="64732B7933574D494571665135736B574C41456B52773D3D"
 HOSTNAME=$(hostname)
 TIME=$(date +"%F %T")
 TITLE="[ATTENTION] $HOSTNAME RESTARTED."
 MESSAGE="At $TIME $HOSTNAME restarted."
 
-echo "${MESSAGE}" | EMAIL="CY EYES <no-reply@clickyab.com>" mutt -s "${TITLE}" -- sysadmin@clickyab.com
+echo "${MESSAGE}" | EMAIL="CY EYES <no-reply@clickyab.com>" mutt -s "${TITLE}" -- ${EMAIL_RECEPTOR}
 
+curl -X POST http://api.kavenegar.com/v1/${KN_API_KEY}/sms/send.json --data-urlencode "receptor=${SMS_RECEPTOR}" --data-urlencode "message=${TITLE} ${MESSAGE}"
